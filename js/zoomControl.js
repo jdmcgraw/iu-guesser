d8d884fc17d4b9ef9e257de4b256aa51f4edfc8c
Joshua McGraw <76073531+jdmcgraw@users.noreply.github.com>
2024-08-25 18:33:36 -0400

    debugging zooming

--- js/zoomControl.js
+++ js/zoomControl.js
@@ -12,11 +12,15 @@ document.addEventListener("DOMContentLoaded", function () {
 
   let zoom = 1;
   const ZOOM_SPEED = 0.1;
+  const MAX_ZOOM = 4;
 
   document.addEventListener("wheel", function (e) {
       e.preventDefault();
 
       const rect = zoomElement.getBoundingClientRect();
+      // Print out the Top Left and Bottom Right coordinates of the image
+      console.log(rect.left, rect.top);
+
       const offsetX = (e.clientX - rect.left) / rect.width; // Normalize by width
       const offsetY = (e.clientY - rect.top) / rect.height; // Normalize by height
 
@@ -26,20 +30,25 @@ document.addEventListener("DOMContentLoaded", function () {
         zoom += ZOOM_SPEED;
       }
 
-      zoom = Math.min(Math.max(zoom, 1), 3);
+      console.log(zoom);
+      zoom = Math.min(Math.max(zoom, 1), MAX_ZOOM);
+
       zoomElement.style.transform = `scale(${zoom})`;
       zoomElement.style.transformOrigin = `${offsetX * 100}% ${offsetY * 100}%`;
 
       var guessPin = document.getElementById('guess-pin');
-      console.log('guess-pin:', guessPin);
-      // If guessPin exists, update its position accordingly to the scale and origin
+      
       if (guessPin) {
-        const rect = zoomElement.getBoundingClientRect();
+        // Get the Position of the Pin
+        var guessPinX = guessPin.style.left;
+        var guessPinY = guessPin.style.top;
 
-        const x = guessPin.x * zoom;
-        const y = guessPin.y * zoom;
-        guessPin.style.left = `${x - pinWidth / 2}px`;
-        guessPin.style.top = `${y - (pinHeight + 3)}px`;
+        // Account for the Zoom too
+        guessPin.style.left = `${(guessPinX - rect.left) - pinWidth / 2 * zoom}px`;
+        guessPin.style.top = `${(guessPinY - rect.top) - (pinHeight + 3) * zoom}px`;
+
+        // TODO: Adjust the Pin's Position when the map rect is transformed as well
+        guessPin.style.transformOrigin = `${offsetX * 100}% ${offsetY * 100}%`;
       }
     },
     { passive: false }
