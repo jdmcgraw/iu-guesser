07dabfa4b665b454b5d037534a4f97d2d4d705f7
Joshua McGraw <76073531+jdmcgraw@users.noreply.github.com>
2024-08-25 18:16:33 -0400

    chore: Update transition duration for all attributes in styles.css

--- js/scripts.js
+++ js/scripts.js
@@ -8,10 +8,10 @@ const pinHeight = 24; // Should match .map-pin height in CSS
 const top_left = {x: 39.163060, y: -86.531024};
 const bottom_right = {x: 39.163060, y: -86.531024};
 
+const mapImage = document.getElementById("map");
+const testimage = document.getElementById("testImage");
+
 document.addEventListener("DOMContentLoaded", function () {
-    
-    const mapImage = document.getElementById("map");
-    const testimage = document.getElementById("testImage");
 
     const imageContainer = document.getElementById("image-container");
 
@@ -35,7 +35,7 @@ document.addEventListener("DOMContentLoaded", function () {
     // Set up to handle image clicks
     mapImage.addEventListener("click", function (event) {
         const rect = mapImage.getBoundingClientRect(); // Get bounding box of image
-        showLockButton(rect); // Call handleButton function from lockInButton.js
+        showLockButton(rect); // Call showLockButton function from lockInButton.js
 
         const x = event.clientX - rect.left;
         const y = event.clientY - rect.top;
