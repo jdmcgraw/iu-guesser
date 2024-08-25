d3e08151470d766a698408f0eded4f530b084320
elijah7000 <143031360+Elijah-J-Kim@users.noreply.github.com>
2024-08-25 15:50:18 -0400

    123

---
+++ js/Images.js
@@ -0,0 +1,59 @@
+import rose_well_house from "../images/rose_well_house.jpg";
+import maxwell from "../images/maxwell.jpg";
+import kirkwood from "../images/kirkwood.jpg";
+import good_body from "../images/good_body";
+import event_product from "../images/bulding_clock";
+import auditorium from "../images/auditorium.jpg";
+import arts_plaza from "../images/arts_plaza.jpg";
+
+export const imagesList = [
+  {
+    id: 1,
+    name: "rose_well_house",
+    src: rose_well_house,
+    lat: 0,
+    long: 0,
+  },
+  {
+    id: 2,
+    name: "maxwell",
+    src: maxwell,
+    lat: 0,
+    long: 0,
+  },
+  {
+    id: 3,
+    name: "kirkwood",
+    src: kirkwood,
+    lat: 0,
+    long: 0,
+  },
+  {
+    id: 4,
+    name: "good_body",
+    src: good_body,
+    lat: 0,
+    long: 0,
+  },
+  {
+    id: 5,
+    name: "event_product",
+    src: event_product,
+    lat: 0,
+    long: 0,
+  },
+  {
+    id: 6,
+    name: "auditorium",
+    src: auditorium,
+    lat: 0,
+    long: 0,
+  },
+  {
+    id: 7,
+    name: "arts_plaza",
+    src: arts_plaza,
+    lat: 0,
+    long: 0,
+  },
+];
